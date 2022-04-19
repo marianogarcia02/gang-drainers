c2ce7f27aa5b7f5c6644e101541b12f8391a1fb7
marianogarcia02 <mariano.ga02@gmail.com>
2022-04-18 19:07:35 -0500

    Update FrogBall.ino

--- FrogBall/FrogBall.ino
+++ FrogBall/FrogBall.ino
@@ -1,3 +1,25 @@
+
+#include <Servo.h>
+const int BUTTONPIN8 = 8;
+const int BUTTONPIN9 = 9;
+
+const int SWITCHPIN2 = 2;
+const int SWITCHPIN3 = 3;
+const int SWITCHPIN4 = 4;
+
+const int SERVOPIN5 = 5;
+const int SERVOPIN6 = 6;
+const int SERVOPIN7 = 7;
+
+int switchValue2;
+int switchValue3;
+int switchValue4;
+
+int buttonValue8;
+int buttonValue9;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
